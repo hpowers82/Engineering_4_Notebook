053fa5fbb82bfee28e92822373017996aa116b74
hpowers82 <60944294+hpowers82@users.noreply.github.com>
2021-09-15 19:52:59 -0400

    Update Automatic_Dice_Roller_Spicy.py
    working spicy version

--- Automatic_Dice_Roller_Spicy.py
+++ Automatic_Dice_Roller_Spicy.py
@@ -1,24 +1,27 @@
 '''
-
                             Online Python Compiler.
                 Code, Compile, Run and Debug python program online.
 Write your code in this editor and press "Run" button to execute it.
-
 '''
 
-print("Hello World")
 from random import randint
 print ("Automatic Dice Roller")
+print ("How many dice?")
+d=input("")
+print ("How many sides?")
+s=input("")
+stop=0
+while stop !="x":
 
+   # print("Press enter to roll, or x to exit!")
+   # n=input("")
+    for x in range (0,int(d)):
+        print("Your number is...%s!" % randint(1,int(s)))
+    print ("Press enter to roll, c then enter to change the ammount of dice, or x then enter to exit!")
+    stop=input("")
+    if stop == "c":
+        print ("How many dice?")
+        d=input("")
+        print ("How many sides?")
+        s=input("")
 
-#n=input("Press enter to roll, or press x to exit!")
-#print(n)
-while n !="x":
-    print ("How many dice?")
-    d=input("")
-    print ("How many sides?")
-    s=input("")
-    print("Press enter to roll, or x to exit!")
-    n=input("")
-    while n
-    print("Your number is...%s!" % randint(1,6))
