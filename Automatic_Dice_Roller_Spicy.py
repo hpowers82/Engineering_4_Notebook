d32f41d92766821d67891a0396bb89ebc86bb3dd
hpowers82 <60944294+hpowers82@users.noreply.github.com>
2021-09-15 20:36:22 -0400

    Update Automatic_Dice_Roller_Spicy.py
    clarity

--- Automatic_Dice_Roller_Spicy.py
+++ Automatic_Dice_Roller_Spicy.py
@@ -7,16 +7,16 @@ print ("Automatic Dice Roller")
 
 while stop !="x":
     if stop == "c":
-        print ("How many dice?")
+        print ("Type how many dice you want to roll, then press enter.")
         d=input("")
-        print ("How many sides?")
+        print ("Type how many sides you want each dice to have, then press enter.")
         s=input("")
         print("rolling %s dice with %s side(s)!" % (d,s))
     for x in range (0,int(d)):
         dicenumb += 1
         print("Dice # %s is...%s!" % (dicenumb,randint(1,int(s))))
     dicenumb=0
-    print("Press enter to roll, c then enter to change the ammount of dice, or x then enter to exit!")
+    print("Press enter to roll, c then enter to change the amount of dice and sides, or x then enter to exit!")
     stop=input("")
 
 
