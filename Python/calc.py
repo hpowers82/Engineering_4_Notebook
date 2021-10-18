d2f0b0bb59a311748479fff13a7c4cc16ef6ce8c
hpowers82 <60944294+hpowers82@users.noreply.github.com>
2021-10-18 12:23:33 -0400

    Update calc.py

--- Python/calc.py
+++ Python/calc.py
@@ -1,10 +1,10 @@
 print("Calculator Program")
 repeat=0
 a = int(input("Enter your first number:"))
-b = int(input("Enter your second number"))
-def doMath(a,b,c):
+b = int(input("Enter your second number")) #int is a number
+def doMath(a,b,c): #import relevent variables: c decides what type of math you want to do.
     if c == 1:
-        return str(a+b)
+        return str(a+b) #str makes the answer a phrase, not just a raw number
     if c == 2:
         return str(a-b)
     if c == 3:
@@ -13,7 +13,6 @@ def doMath(a,b,c):
         return str(a/b)
     if c == 5:
         return str(a%b)
-#while repeat!="x"
 
 print("Sum:\t\t" + doMath(a,b,1))
 print("Difference:\t" + doMath(a,b,2))
