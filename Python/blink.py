70f8376b2bb080924bd95ad273515d4309d19b41
hpowers82 <hpowers82@charlottesvilleschools.org>
2021-11-22 16:35:49 +0000

    cooo

--- Python/blink.py
+++ Python/blink.py
@@ -6,7 +6,13 @@ GPIO.setup(12, GPIO.OUT)
 def goLed(x,y):
 	GPIO.output(21,x)
 	GPIO.output(12,y)
+	if  x == 1:
+		print("green on!")
+	elif y == 1:
+		print("red on!")
 	sleep(.25)
 while True:
 	goLed(1,0)
+	print("green on!")
 	goLed(0,1)
+	print("red on!")
