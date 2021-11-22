2727d26a4f7467b1395baba46a68a7cf958dc381
hpowers82 <60944294+hpowers82@users.noreply.github.com>
2021-11-22 11:45:26 -0500

    Update blink.py

--- Python/blink.py
+++ Python/blink.py
@@ -1,18 +1,17 @@
-import RPi.GPIO as GPIO
-from time import sleep
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(21, GPIO.OUT)
+import RPi.GPIO as GPIO #imports easier way to talk to pins
+from time import sleep #imports the ability to have it slow down for a period
+GPIO.setmode(GPIO.BCM) #sets the numbering of pins on cobbler correctly
+GPIO.setup(21, GPIO.OUT) #sets pin 21 as an output
 GPIO.setup(12, GPIO.OUT)
-def goLed(x,y):
-	GPIO.output(21,x)
-	GPIO.output(12,y)
-	if  x == 1:
+def 
+def goLed(x,y,z):
+	GPIO.output(21,x) #sets the status of the pin as either having a high status (led is on) or low status (led is off)
+	if x = 1:
 		print("green on!")
-	elif y == 1:
-		print("red on!")
+	GPIO.output(12,y) #does the same with the other pin
+	if y = 1:
+		print("green on!"
 	sleep(.25)
 while True:
 	goLed(1,0)
-	print("green on!")
 	goLed(0,1)
-	print("red on!")
