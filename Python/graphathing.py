5ebd79b77b38a6414c8a008f18bf0af18d24acc8
hpowers82 <60944294+hpowers82@users.noreply.github.com>
2021-12-07 11:18:14 -0500

    wow

--- Python/graphathing.py
+++ Python/graphathing.py
@@ -23,39 +23,33 @@ bottom = height-padding
 
 pos_x = 0
 max_x = 0
+l = 128
+h=64  
 disp.begin()
 pixelList = []
 font = ImageFont.load_default()
 draw = ImageDraw.Draw(image)
-#128, 64
-
+draw.rectangle((0,0,l,h), outline=0, fill=0)
 while True:
-  draw.rectangle((0,0,width,height), outline=0, fill=0)
   accel, mag = lsm303.read()
   accel_x, accel_y, accel_z = accel
   mag_x, mag_y, mag_z = mag
   
-  pos_x += accel_x
-  
-  #y = round((max_x/64) * pos_x)
-  
-  # pixelList = pixelList + y
-  if len(pixelList) > 128:
+  pixelList = pixelList + accel_x
+  if len(pixelList) > l:
     pixelList.pop(0)
 
-  if pos_x > max_x:
-    max_x = pos_x
-    redo = time
-    while redo < 0:
-      draw.rectangle((0,0,width,height), outline=0, fill=0)
-      display.pixel((redo-1),round(max_x/64) * pos_x)
-      y = round((max_x/64) * pos_x)
-                            
-  display.pixel(time, y)
-
-  display.pixel(timer,
-  draw.text((x,top+20), "y: " + (str(round(accel_y / 107, 3))), font=font, fill=225)
-  #draw.text((x,top+20), 'Accel Y='+str(accel_y), font=font, fill=225)
- # draw.text((x,top+40), 'Accel Z='+str(accel_z), font=font, fill=225)
-  #time.sleep(.5)
+  if accel_x > max_x:
+    max_x = accel_x
+    redo = 0
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
+    
+    while redo <= time:
+      draw.pixel((redo),(round(max_x/h) * pixelList[redo]))
+      redo += 1
+    elif:  
+      y = round((max_x/h) * pos_x)
+      draw.pixel(time, y)
+  if time != 128:
+    time += 1
   disp.image(image)
