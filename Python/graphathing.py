b6d80535f32e384709ff4672e99dc7e318437ac3
hpowers82 <60944294+hpowers82@users.noreply.github.com>
2021-12-09 11:52:53 -0500

    Update graphathing.py

--- Python/graphathing.py
+++ Python/graphathing.py
@@ -22,40 +22,43 @@ bottom = height-padding
 pos_x = 0
 max_x = 1
 time = 0
-l = 128
-h=64  
+width = 128
+height = 64  
 disp.begin()
 pixelList = []
 font = ImageFont.load_default()
 draw = ImageDraw.Draw(image)
-draw.rectangle((0,0,l,h), outline=0, fill=0)
+def getY(pixelList,redo):
+  y=round((int(pixelList[redo])/int(max(pixelList))*64)
+  return y         
 while True:
-
+  time += 1
   accel, mag = lsm303.read()
   accel_x, accel_y, accel_z = accel
   mag_x, mag_y, mag_z = mag
   accel_x=abs(accel_x)
   print(accel_x)
   redo = 0
-  y=round((accel_x/max_x)*64)
-  
+  print(getY(pixelList,-1)) 
   pixelList.append(accel_x)
-  
-  
   if len(pixelList) > l:
     pixelList.pop(0)
+  max_x = max(pixelList)
+          
+  if time <= 128:
+    redo = 128
+    for i in len(pixelList):
+      getY()
+      draw.rectangle((redo, y ,1,1), outline=225,fill=225)
+      redo -= 1
+    redo = 0
     
-  if accel_x > max_x:
-    max_x = accel_x
-    y=round((accel_x/max_x)*64)
-  if len(pixelList) >= 128:
+  if len(pixelList) > 128:
     draw.rectangle((0,0,disp.width,disp.height), outline=0, fill=0)
-    while redo <= 128:
-        draw.rectangle((redo, round((int(pixelList[redo])/max_x) * 64) ,1,1), outline=225,fill=225)
+    redo = 0
+    for i in 128:
+        findY()
+        draw.rectangle((redo,y,1,1), outline=225,fill=225)
         redo += 1
-  else:
-    draw.rectangle((time,y,1,1), outline=1, fill=1)
-    print("time is:" +str(y)+"\nY is: "+str(y))
-    time += 1
     
   disp.image(image)
