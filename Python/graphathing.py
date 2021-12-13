e279a3453116e766f5b9ed9b6d1da092c8b5dc56
hpowers82 <60944294+hpowers82@users.noreply.github.com>
2021-12-13 13:04:21 -0500

    Update graphathing.py

--- Python/graphathing.py
+++ Python/graphathing.py
@@ -10,7 +10,6 @@ lsm303 = Adafruit_LSM303.LSM303()
 x=0
 RST = 24
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
-
 width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
@@ -26,6 +25,8 @@ width = 128 ## first input in print is width
 height = 64  ##  second input in print is height
 disp.begin()
 pixelList = []
+for i in range(128):
+ pixelListList.append(0)
 font = ImageFont.load_default()
 draw = ImageDraw.Draw(image)
 def getY(pixelList,redo):
@@ -41,24 +42,31 @@ while True:
   print(accel_x)
   redo = 0 
   pixelList.append(accel_x)
-  if len(pixelList) > 128:
-    pixelList.pop(0)
-  max_x = max(pixelList)
-          
-  if time < 128:
-    redo = 128
-    for i in range(len(pixelList)):
-      getY()
-      draw.rectangle((redo,y,redo,y), outline=225,fill=225)
-      redo -= 1
-    redo = 0
+  pixelList.pop(0)
+  
+  while redo < 128:
+    getY()
+    draw.rectangle((redo,y,redo,y), outline=225,fill=225)
+  redo += 1
+
+
+
+      
+  # time < 128:
+   # redo = 128
+#    #for i in range(time):
+ #     if pixelList[redo] = 0
+  #    getY()
+   #   draw.rectangle((redo,y,redo,y), outline=225,fill=225)
+ #     redo += 1
+ #   redo = 0
     
-  if len(pixelList) >= 128:
-    draw.rectangle((0,0,disp.width,disp.height), outline=0, fill=0)
-    redo = 0
-    for i in 128:
-        findY()
-        draw.rectangle((redo,y,redo,y), outline=225,fill=225)
-        redo += 1
+ # if len(pixelList) >= 128:
+ #   draw.rectangle((0,0,disp.width,disp.height), outline=0, fill=0)
+  #  redo = 0
+   # for i in 128:
+   #     findY()
+    #    draw.rectangle((redo,y,redo,y), outline=225,fill=225)
+     #   redo += 1
     
   disp.image(image)
