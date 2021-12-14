a35d35312b71022f95c9e68d225bf839b3109990
hpowers82 <60944294+hpowers82@users.noreply.github.com>
2021-12-14 13:41:46 -0500

    Update graphathing.py

--- Python/graphathing.py
+++ Python/graphathing.py
@@ -33,7 +33,7 @@ font = ImageFont.load_default()
 draw = ImageDraw.Draw(image)
 #draw.text((64,32), "epic", font=font, fill=225)
 draw.rectangle((0,0,width,height), outline=255, fill=255)
-disp.display
+disp.display()
 time.sleep(2)
 while True:
   draw.rectangle((0,0,width,height), outline=0, fill=0)
