b299075c5b1206597bce1d0fec7a97240cf970e4
hpowers82 <60944294+hpowers82@users.noreply.github.com>
2021-12-14 12:00:00 -0500

    Update graphathing.py

--- Python/graphathing.py
+++ Python/graphathing.py
@@ -28,29 +28,24 @@ for i in range(128):
  pixelList.append(0)
 font = ImageFont.load_default()
 draw = ImageDraw.Draw(image)
-def getY(pixelList,redo):
- # y=round((int(pixelList[redo])/int(max(pixelList))*64)
-  y=pixelList[redo]/max(pixelList)
-  return y
 while True:
   draw.rectangle((0,0,width,height), outline=0, fill=0)
-  time += 1
   accel, mag = lsm303.read()
   accel_x, accel_y, accel_z = accel
   mag_x, mag_y, mag_z = mag
   accel_x=abs(accel_x)
-  print(accel_x)
+  #print(accel_x)
   pixelList.append(accel_x)
   pixelList.pop(0)
-  print(pixelList)
+  #print(pixelList)
   #y=round(pixelList[127]/max(pixelList)*64,0)
   #print(y)
   while redo < 128:
     y=round(pixelList[redo]/max(pixelList)*64,0)
     draw.rectangle((redo,y,redo,y), outline=225,fill=225)
     redo += 1
-    #print(redo)
-    #print(y)
+    print(redo)
+    print(y)
   redo = 0
 
 
