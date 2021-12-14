7b70ab9ea91fe010a0dfaef0d1fdc8d4d8cfe1a9
hpowers82 <60944294+hpowers82@users.noreply.github.com>
2021-12-14 11:50:50 -0500

    Update graphathing.py

--- Python/graphathing.py
+++ Python/graphathing.py
@@ -43,7 +43,7 @@ while True:
   pixelList.append(accel_x)
   pixelList.pop(0)
   print(pixelList)
-  y=round(pixelList[redo]/max(pixelList))*64
+  y=round(pixelList[redo]/max(pixelList))*64,1)
   print(y)
   while redo < 128:
     y=(pixelList[redo]/max(pixelList))*64
