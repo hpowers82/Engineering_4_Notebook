dc032fd5fcb5452b9ffc32f93031719918e21d87
hpowers82 <60944294+hpowers82@users.noreply.github.com>
2022-05-24 12:32:31 -0400

    Update floatControl.py

--- Python/floatControl.py
+++ Python/floatControl.py
@@ -1,12 +1,31 @@
 import time
-liftoff=time.time()
-import Adafruit_LSM303
-import Adafruit_GPIO.SPI as SPI
+import adafruit_mpl3115a2
+import board
+from gpiozero import Servo
 
-lsm303 = Adafruit_LSM303.LSM303()
-dist = 0
+sensor = adafruit_mpl3115a2.MPL3115A2(i2c, address=0x60)
+sensor.sealevel_pressure = 102250
+i2c = board.I2C()
+servo = Servo(18)
+
+deflate = False
+
+initialTime = time.time()
+initialAlt = sensor.altitude
+
+servo.max
+altitudeRec[]
 while True:
-  accel, mag = lsm303.read()
-  accel_x, accel_y, accel_z = accel
-  mag_x, mag_y, mag_z = mag
+  altitude = sensor.altitude
+  temperature = sensor.temperature
+  pressure = sensor.pressure
+  altitudeRec.append(sensor.altitude)
+  if altitude >= initialAlt + 20 and deflate = False:
+    servo.min
+    deflate = True
+  elif altitude >= initialAlt + 20 and deflate = True:
+    time.sleep(3)
+  if deflate = True and max(altitudeRec) 
+    
+    
   
