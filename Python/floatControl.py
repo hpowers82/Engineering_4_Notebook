0425286080bd3b0c19d1fdbe90dbe9cd7aed1653
hpowers82 <60944294+hpowers82@users.noreply.github.com>
2022-05-26 12:49:33 -0400

    Update floatControl.py

--- Python/floatControl.py
+++ Python/floatControl.py
@@ -1,31 +1,36 @@
 import time
 import adafruit_mpl3115a2
 import board
-from gpiozero import Servo
+from gpiozero import Servo, Button
+import os
+import picamera
+
 
 sensor = adafruit_mpl3115a2.MPL3115A2(i2c, address=0x60)
 sensor.sealevel_pressure = 102250
 i2c = board.I2C()
 servo = Servo(18)
-
-deflate = False
-
+directory = "../pictures"
 initialTime = time.time()
-initialAlt = sensor.altitude
+camera = picamera.PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview()
 
-servo.max
+servo.max # makes sure that the servo is in place
 altitudeRec[]
+tempratureRec[]
+pressureRec[]
+
 while True:
   altitude = sensor.altitude
   temperature = sensor.temperature
   pressure = sensor.pressure
   altitudeRec.append(sensor.altitude)
-  if altitude >= initialAlt + 20 and deflate = False:
+  tempratureRec.append(sensor.temprature)
+  pressureRec.append(sensor.pressure) # records data
+  if altitudeRec[-1] >= altitudeRec[0] + 20 or time.time() < initialTime: # defines the maximum altitude, and deflates the variable balloon once it reaches it.
+    camera.capture('../pictures/image + '.jpg')
     servo.min
-    deflate = True
-  elif altitude >= initialAlt + 20 and deflate = True:
-    time.sleep(3)
-  if deflate = True and max(altitudeRec) 
-    
+
     
   
